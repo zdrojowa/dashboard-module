85eacaa7af36cf4289d934c10223bec049cae8ad
Jakub Kosiński <jkosinski@zdrojowainvest.pl>
2019-12-16 16:14:53 +0100

    some fixes

--- resources/assets/js/DashboardModule.js
+++ resources/assets/js/DashboardModule.js
@@ -6,45 +6,51 @@ require('./chosenjs.min.js');
 require('./components/ZdrojowaTable/ZdrojowaTable');
 require('./components/CircleBar/script');
 
-$('.nav-link.active').each(function () {
-    const closest = $(this).closest('div.collapse');
-    closest.slideDown(150, 'swing').addClass('show');
-    closest.siblings('a.nav-link.collapsed').removeClass('collapsed').addClass('active');
+$(function () {
+
+
+    $.fn.collapseLink = function (siblingsEl) {
+        if ($(this)[0] !== siblingsEl.siblings('.collapse')[0]) {
+            $(this).slideUp(150, 'swing').removeClass('show');
+            $(this).siblings('.nav-link').addClass('collapsed');
+        }
+    };
+
+
+    $('.nav-link.active').each(function () {
+        const closest = $(this).closest('div.collapse');
+        closest.slideDown(150, 'swing').addClass('show');
+        closest.siblings('a.nav-link.collapsed').removeClass('collapsed').addClass('active');
+    });
+
+    $('.nav-link').click(function () {
+        $('.collapse.show').collapseLink($(this));
+
+        if ($(this).hasClass('collapsed')) {
+            $(this).siblings('.collapse').slideDown(150, 'swing').addClass('show');
+            $(this).removeClass('collapsed');
+        } else {
+            $(this).siblings('.collapse.show').slideUp(150, 'swing').removeClass('show');
+            $(this).addClass('collapsed');
+        }
+    });
+
+
+    $(".nav-link").hover(function () {
+        const parent = $(this).parent().parent().parent().parent();
+
+        if (parent.hasClass('nav-item')) {
+            parent.find(".nav-link").eq(0).addClass("hover-active");
+        }
+    }, function () {
+        const parent = $(this).parent().parent().parent().parent();
+
+        if (parent.hasClass('nav-item')) {
+            parent.find(".nav-link").eq(0).removeClass("hover-active");
+        }
+    });
+
 });
-
-$('.nav-link').click(function () {
-    $('.collapse.show').collapseLink($(this));
-
-    if ($(this).hasClass('collapsed')) {
-        $(this).siblings('.collapse').slideDown(150, 'swing').addClass('show');
-        $(this).removeClass('collapsed');
-    } else {
-        $(this).siblings('.collapse.show').slideUp(150, 'swing').removeClass('show');
-        $(this).addClass('collapsed');
-    }
-});
-
-$.fn.collapseLink = function (siblingsEl) {
-    if ($(this)[0] !== siblingsEl.siblings('.collapse')[0]) {
-        $(this).slideUp(150, 'swing').removeClass('show');
-        $(this).siblings('.nav-link').addClass('collapsed');
-    }
-};
-
-$(".nav-link").hover(function () {
-    const parent = $(this).parent().parent().parent().parent();
-
-    if(parent.hasClass('nav-item')) {
-        parent.find(".nav-link").eq(0).addClass("hover-active");
-    }
-}, function () {
-    const parent = $(this).parent().parent().parent().parent();
-
-    if(parent.hasClass('nav-item')) {
-        parent.find(".nav-link").eq(0).removeClass("hover-active");
-    }
-});
-
 (function($) {
     $.fn.button = function(action) {
         if (action === 'loading' && this.data('loading-text')) {
